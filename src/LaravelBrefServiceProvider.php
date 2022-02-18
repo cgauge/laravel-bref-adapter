98f66a950f6837cd416983fbd5e23b825d79dc6c
Deleu <deleugyn@gmail.com>
2022-02-18 12:05:19 +0100

    add explanation for why sometimes we don't bind the Context on Service Provider

--- src/LaravelBrefServiceProvider.php
+++ src/LaravelBrefServiceProvider.php
@@ -38,11 +38,19 @@ final class LaravelBrefServiceProvider extends ServiceProvider
 
     private function brefContext(): void
     {
+        // When using Bref Function instead of Bref FPM, there will be no SERVER header
+        // because we already are inside the runtime execution. That means the callable
+        // function has received the Context and should be able to bind it directly.
+        // see https://github.com/cgauge/laravel-bref-adapter/blob/dfafb6f204d8d725f427e47906549cad18f66c69/handlers/request.php#L11
         if ($this->app->has(Context::class)) {
             return;
         }
 
         $this->app->bind(Context::class, function () {
+            // When using Bref with FPM Layer, Bref will json_encode the Execution Context
+            // and inject it as a SERVER header on FPM so that we (users) can retrieve
+            // and use it. That means we can decode and reconstruct the original
+            // Bref Context object.
             if (isset($_SERVER['LAMBDA_INVOCATION_CONTEXT'])) {
                 $lambdaContext = json_decode($_SERVER['LAMBDA_INVOCATION_CONTEXT'], true);
             } else {
