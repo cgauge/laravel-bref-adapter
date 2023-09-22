b75ee5db553f6370f68e7b4241dbf296eafaa9dd
Marco Aurélio Deleu <deleugyn@gmail.com>
2023-09-22 10:04:23 -0300

    Supress exception

--- src/Kernel/SqsHandlerTesting.php
+++ src/Kernel/SqsHandlerTesting.php
@@ -6,19 +6,19 @@ use Bref\Context\Context;
 use Bref\Event\Sqs\SqsRecord;
 use CustomerGauge\Bref\Queue\LambdaJob;
 use Illuminate\Contracts\Container\Container;
-use Illuminate\Queue\Jobs\Job;
 use Illuminate\Queue\Queue;
+use Throwable;
 
 final class SqsHandlerTesting
 {
     private $container;
 
-    public function __construct(Container  $container)
+    public function __construct(Container $container)
     {
         $this->container = $container;
     }
 
-    public function dispatchSqsJob(Job $job)
+    public function dispatchSqsJob($job, bool $suppressException = false)
     {
         $queue = new class extends Queue {
             public function createPayload($job, $queue, $data = '')
@@ -37,6 +37,12 @@ final class SqsHandlerTesting
         /** @var SqsHandler $sqs */
         $sqs = $this->container->make(SqsHandler::class);
 
-        $sqs->run(new LambdaJob($this->container, $sqsMessage), Context::fake());
+        try {
+            $sqs->run(new LambdaJob($this->container, $sqsMessage), Context::fake());
+        } catch (Throwable $t) {
+            if (! $suppressException) {
+                throw $t;
+            }
+        }
     }
 }
